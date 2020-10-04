6c38de52d2b3af12a95d156f7dda07c5290a82ba
Denis Erofeev <edenisn@gmail.com>
2020-10-04 16:29:06 +0400

    func omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    const resultObj = {};
+    for(let value of Object.entries(obj)) {
+        if(!fields.includes(value[0])) {
+            resultObj[value[0]] = value[1];
+        }
+    }
 
+    return resultObj;
 };
