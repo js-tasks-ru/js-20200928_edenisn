388b776f040d54d82d625e23785adb36f7916343
Denis Erofeev <edenisn@gmail.com>
2020-10-07 18:17:27 +0400

    code improvements

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,15 +4,5 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-    const uniqArr = [];
-    if (arr) {
-        for (let i = 0; i < arr.length; i++) {
-            const element = arr[i];
-            if ( !uniqArr.includes(element) ) {
-                uniqArr.push(element);
-            }
-        }
-    }
-
-    return uniqArr;
+    return [...new Set(arr)];
 }
