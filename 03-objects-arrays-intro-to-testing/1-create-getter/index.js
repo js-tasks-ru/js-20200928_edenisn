376c25a6676e12c6191ce58063f2cdec49c2e393
Denis Erofeev <edenisn@gmail.com>
2020-10-07 20:38:19 +0400

    more code improvements

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -7,14 +7,20 @@ export function createGetter(path) {
     const pathArr = path.split('.');
 
     return function(obj) {
-        let tempObj = obj;
-        for (const elem of pathArr) {
-            if (!tempObj) {
-                break;
-            }
-            tempObj = tempObj[elem];
-        }
-
-        return tempObj;
+        return iterate(obj, pathArr);
     };
+
+    function iterate(obj, pathArr) {
+        for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+                if (typeof obj[property] === "object") {
+                    return iterate(obj[property], pathArr);
+                } else {
+                    if (pathArr.includes(property)) {
+                        return obj[property];
+                    }
+                }
+            }
+        }
+    }
 }
