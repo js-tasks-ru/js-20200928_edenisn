b3afbb8ad3c1202daf1e1969eea5b6ed0fda02e6
Denis Erofeev <edenisn@gmail.com>
2020-10-07 18:16:13 +0400

    func implementation: createGetter()

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const pathArr = path.split('.');
 
+    return function(obj) {
+        let tempObj = obj;
+        for (const elem of pathArr) {
+            if (!tempObj) {
+                break;
+            }
+            tempObj = tempObj[elem];
+        }
+
+        return tempObj;
+    };
 }
