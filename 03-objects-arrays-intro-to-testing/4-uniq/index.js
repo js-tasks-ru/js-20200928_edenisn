bdc942d23bd5b8372772b57017bbf9bb64e133aa
Denis Erofeev <edenisn@gmail.com>
2020-10-07 13:54:17 +0400

    func implementation: uniq(arr)

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,15 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+    const uniqArr = [];
+    if (arr) {
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            if ( !uniqArr.includes(element) ) {
+                uniqArr.push(element);
+            }
+        }
+    }
 
+    return uniqArr;
 }
