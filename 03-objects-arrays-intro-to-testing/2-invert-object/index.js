9f354cbd64e91e92a11ccdab1b351b5bde9a8158
Denis Erofeev <edenisn@gmail.com>
2020-10-07 13:35:29 +0400

    func impementation: invertObj(obj)

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    const resultObj = {};
+    if (obj) {
+        for (const [key, value] of Object.entries(obj)) {
+            resultObj[value] = key;
+        }
 
+        return resultObj;
+    }
 }
